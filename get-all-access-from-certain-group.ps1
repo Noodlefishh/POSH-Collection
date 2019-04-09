93d1fda7b9cc86c9c34ee8ca13437c0a8314ac53
Jonathan Bourbonnais <49311040+jonathanbourbonnais@users.noreply.github.com>
2019-04-09 15:42:07 -0400

    Find in the Active Directory a group with certain keywords
    This script enable you to crawl in the AD to find groups with keywords.

---
+++ get-all-access-from-certain-group.ps1
@@ -0,0 +1,9 @@
+﻿Get-ADGroup -Filter {name -like 'Enter a keywork for the AD group*'} | 
+
+ForEach-Object {
+    Get-ADGroupMember $_.name | 
+ForEach-Object {
+    Get-ADPrincipalGroupMembership $_.samaccountname | Where-Object {($_.name -like ("Enter a first keywork*")) -or ($_.name -like ("Enter a second keyword*"))  }
+}
+
+} | Export-Csv -Path C:\path-to-file\file.csv 
