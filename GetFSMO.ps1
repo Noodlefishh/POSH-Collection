58b7f7d8ecb676e1a7183a68b7858d009b2acf17
Jonathan Bourbonnais <49311040+jonathanbourbonnais@users.noreply.github.com>
2019-04-29 14:41:35 -0400

    Update GetFSMO.ps1

--- GetFSMO.ps1
+++ GetFSMO.ps1
@@ -1,4 +1,4 @@
-﻿#Get the role older for all opf the five FSMO roles
+﻿#Get the roles olders for all of the five FSMO roles and wich server are global catalogs
 
 Import-Module ActiveDirectory
 
@@ -6,3 +6,4 @@ $domain = "yourdomain.com"
 
 Get-ADForest $domain | Format-List SchemaMaster,DomainNamingMaster
 Get-ADDomain $domain | Format-List PDCEMulator,RIDMaster,InfrastructureMaster
+Get-ADForest | format-list -Property GlobalCatalogs 
