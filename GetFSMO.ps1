238c781177c80fb34d1d6032a2db109a6141a25f
Jonathan Bourbonnais <49311040+jonathanbourbonnais@users.noreply.github.com>
2019-04-29 14:37:16 -0400

    Get FSMO roles older
    This script help you get wich server own wich FSMO roles

---
+++ GetFSMO.ps1
@@ -0,0 +1,8 @@
+﻿#Get the role older for all opf the five FSMO roles
+
+Import-Module ActiveDirectory
+
+$domain = "yourdomain.com"
+
+Get-ADForest $domain | Format-List SchemaMaster,DomainNamingMaster
+Get-ADDomain $domain | Format-List PDCEMulator,RIDMaster,InfrastructureMaster
