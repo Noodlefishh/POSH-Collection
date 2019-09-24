f865ec290df4216b01e8e6befe67ced73f702d0d
Jonathan Bourbonnais <49311040+jonathanbourbonnais@users.noreply.github.com>
2019-09-24 12:40:27 -0400

    Add files via upload
    This script enable you to get all the file created in a the last 20 years on a path. 
    Ideal to monitor activity on a file server.

---
+++ Get_All_Files_Created_Every_Years.ps1
@@ -0,0 +1,334 @@
+$fichiers = Get-ChildItem -Path "\\path\to\file" -Recurse 
+
+$Counter = 0
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2019*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2019 : " $Counter
+$Counter = 0
+
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2018*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2018 : " $Counter
+$Counter = 0
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2017*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2017 : " $Counter
+$Counter = 0
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2016*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2016 : " $Counter
+$Counter = 0
+
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2015*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2015 : " $Counter
+$Counter = 0
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2014*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2014 : " $Counter
+$Counter = 0
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2013*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2013 : " $Counter
+$Counter = 0
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2012*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2012 : " $Counter
+$Counter = 0
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2011*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2011 : " $Counter
+$Counter = 0
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2010*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2010 : " $Counter
+$Counter = 0
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2009*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2009 : " $Counter
+$Counter = 0
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2008*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2008 : " $Counter
+$Counter = 0
+
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2007*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2007 : " $Counter
+$Counter = 0
+
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2006*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2006 : " $Counter
+$Counter = 0
+
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2005*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2005 : " $Counter
+$Counter = 0
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2004*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2004 : " $Counter
+$Counter = 0
+
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2003*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2003 : " $Counter
+$Counter = 0
+
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2002*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2002 : " $Counter
+$Counter = 0
+
+
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2001*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2001 : " $Counter
+$Counter = 0
+
+
+
+
+
+
+$fichiers | ForEach-Object {
+
+    $fichier = $_
+
+if ($fichier.CreationTime -like "*2000*") {
+
+    $Counter++
+
+  }
+
+}
+Write-Host "2000 : " $Counter
+$Counter = 0
